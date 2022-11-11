96a67663e4956eee561a894e23408ac78c23898f
sadcapybara137 <114882106+sadcapybara137@users.noreply.github.com>
2022-11-11 12:44:20 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,21 +12,46 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
-  return (
+  const [cart, setCart] = useState([]);
+  const [cartnames, setCartNames] = useState([]);
+  const [total, setTotal] = useState(0);
+  const names = ["Cookies", "Matcha Mille Crepe", "Egg Boi", "Ube Boi", "Brownies", "Macarons","Bread", "Croissant",
+                "Bread with Butter", "Cake Lad", "Tart", "Bagel", "Gingerbread Cookie", "Cheesecake", "Creampuff"]
+    function updateCart(name, price) {
+        setCart([...cart, price])
+        setCartNames([...cartnames, name])
+        setTotal(total + price)
+    }
+    return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item, index) => (
+          <div>
+              <BakeryItem url={item.image} price={setPrice(index)} name={names[index]}/>
+              <button onClick={() => updateCart(names[index], setPrice(index))}> Add to cart! </button>
+          </div>
+          // TODO: map bakeryData to BakeryItem components
       ))}
 
       <div>
         <h2>Cart</h2>
+          {cart.map((price, index) => (
+              <p> {cartnames[index]} ${price}</p>
+          ))}
+          <p> Total Cost: ${Math.round(total*100) / 100}</p>
+
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
   );
+}
+
+function setPrice(index) {
+    if(index % 2 === 1) {
+        return 7.99;
+    } else {
+        return 2.99;
+    }
 }
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,9 @@
 // TODO: create a component that displays a single bakery item
+import updateCart from "../App";
+
+export default function BakeryItem(props) {
+    return <div>
+                <img src = {props.url}/>
+                <p> {props.name} Price: ${props.price}</p>
+           </div>
+}
